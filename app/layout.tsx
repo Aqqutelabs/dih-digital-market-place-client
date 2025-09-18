b3ed77d1b324db87fe82d5afc9eab96c2387ea8f
estherchinda <davidakachi9@gmail.com>
2025-09-18 20:11:56 +0100

    scaffolded project

---
+++ app/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { Montserrat } from "next/font/google";
+
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Wider NetFarms Digital Innovation Hub",
+  description: "A platform for linking Agrobusinesses",
+  icons: {
+    icon: "/favicon.svg",
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/sign-in/page.tsx
@@ -0,0 +1,11 @@
+export default function SignIn() {
+    return (
+        <section className="h-screen flex justify-center items-center p-[100px] bg-[#F2F7F3]">
+            <div className="flex h-full md:h-[650px] max-h-[700px] w-full">
+                <div className="w-full h-full bg-[#16A249] rounded-l-4xl"></div>
+                <div className="w-full h-full bg-white rounded-r-4xl"></div>
+            </div>
+
+        </section>
+    )
+}
